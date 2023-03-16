3bfba18bb9ae1fab4ba31d4e6b12037eb185d465
dodaxx <fenosoaramaroson9@gmail.com>
2023-03-16 17:36:39 +0300

    Add function clear, save and restore

--- public/app/index.js
+++ public/app/index.js
@@ -1,6 +1,13 @@
 import $ from "jquery";
 const fabric = require("fabric").fabric;
 let isDrawing = false;
+let jsonSave;
+let imageBackground = "";
+var canvas = new fabric.Canvas('canvas', {
+    width: 800,
+    height: 700,
+});
+canvas.setZoom(canvas.getZoom() * 1);
 const handleDrawing = () => {
     isDrawing = !isDrawing;
     if (isDrawing) {
@@ -13,22 +20,38 @@ const handleDrawing = () => {
 const handleImage = (event) => {
     $(".overlay").addClass("show");
     $(".edit-image").addClass("show");
+    const myPromise = new Promise((resolve, reject) => {
+        imageBackground = event.target.currentSrc;
+        resolve(imageBackground);
+    });
+    myPromise.then(image => {
+        fabric.Image.fromURL(image, function (img) {
+            img.scaleToWidth(canvas.width);
+            img.scaleToHeight(canvas.height);
+            canvas.setBackgroundImage(img);
+            canvas.requestRenderAll();
+        });
+    });
 };
-const handleButton = (e) => {
+const handleUtils = (e) => {
     const target = e.target.tagName;
     $(`${target}`).toggleClass('active');
     handleDrawing();
 };
-const handleMouseDown = () => {
-    $("button").toggleClass("mouse-down");
+const handleSave = () => {
+    jsonSave = canvas.toJSON();
+    console.log(jsonSave);
+    canvas.getObjects().forEach((o) => {
+        canvas.remove(o);
+    });
+};
+const handleRestore = () => {
+    canvas.loadFromJSON(jsonSave, canvas.renderAll.bind(canvas));
 };
 $(".image").on("click", handleImage);
-$(".utils-utils").on("click", handleButton);
-const utilsUtils = document.querySelector(".utils-utils");
-// utilsUtils?.addEventListener("click", handleButton)
-var canvas = new fabric.Canvas('canvas', {
-    width: 500,
-    height: 700,
-    backgroundImage: 'https://thumbs.dreamstime.com/z/silhouette-de-femme-13998582.jpg"'
-    // backgroundColor: 'white'
-});
+$(".utils-utils").on("click", handleUtils);
+$(".save button").on("click", handleSave);
+$(".restore button").on("click", handleRestore);
+var text = new fabric.Text('hello world', { left: 100, top: 100 });
+var itext = new fabric.IText('GeeksforGeeks', {});
+canvas.add(text, itext);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-import $, { event } from "jquery";
+import $, { cssNumber, event } from "jquery";
 const fabric = require("fabric").fabric;
 
 let isDrawing = false as boolean;
+let jsonSave: any;
+let imageBackground = "" as string;
+
+var canvas = new fabric.Canvas('canvas', {
+  width: 800,
+  height: 700,
+});
+
+canvas.setZoom(canvas.getZoom() * 1);
 
 const handleDrawing = () => {
   isDrawing = !isDrawing;
@@ -15,35 +24,50 @@ const handleDrawing = () => {
 const handleImage = (event: any): void => {
   $(".overlay").addClass("show");
   $(".edit-image").addClass("show");
+  const myPromise = new Promise((resolve, reject) => {
+    imageBackground = event.target.currentSrc
+    resolve(imageBackground)
+  })
+  myPromise.then(image => {
+    fabric.Image.fromURL(image, function (img: any) {
+      img.scaleToWidth(canvas.width);
+      img.scaleToHeight(canvas.height);
+      canvas.setBackgroundImage(img);
+      canvas.requestRenderAll();
+    });
+  })
 }
 
-const handleButton = (e: any) => {
+const handleUtils = (e: any) => {
   const target = e.target.tagName;
   $(`${target}`).toggleClass('active')
   handleDrawing();
 }
 
-const handleMouseDown = () => {
-  $("button").toggleClass("mouse-down");
 
+const handleSave = () => {
+  jsonSave = canvas.toJSON();
+  console.log(jsonSave)
+  canvas.getObjects().forEach((o: any) => {
+    canvas.remove(o)
+  });
 }
 
+const handleRestore = () => {
+  canvas.loadFromJSON(jsonSave, canvas.renderAll.bind(canvas));
+}
+
+
 $(".image").on("click", handleImage);
+$(".utils-utils").on("click", handleUtils);
+$(".save button").on("click", handleSave);
+$(".restore button").on("click", handleRestore);
 
-$(".utils-utils").on("click", handleButton);
-const utilsUtils = document.querySelector(".utils-utils")
-// utilsUtils?.addEventListener("click", handleButton)
+var text = new fabric.Text('hello world', { left: 100, top: 100 });
 
+var itext = new fabric.IText('GeeksforGeeks', {
 
-
-
-
-
-var canvas = new fabric.Canvas('canvas', {
-  width: 500,
-  height: 700,
-  backgroundImage: 'https://thumbs.dreamstime.com/z/silhouette-de-femme-13998582.jpg"'
-  // backgroundColor: 'white'
 });
 
 
+canvas.add(text, itext);
